fb6e83e7defb3974bfe58240934df62e5d0d1b53
Kamino666 <516015417@qq.com>
2022-06-20 19:17:34 +0800

    优化了trace.py，可以通过-m参数来避免调用tkinter

--- trace.py
+++ trace.py
@@ -2,6 +2,7 @@ import logging
 import tkinter as tk
 import tkinter.filedialog
 from pathlib import Path
+import argparse
 
 import cv2
 import matplotlib
@@ -48,14 +49,16 @@ def detect_watermark_from_img_result(img, res, err_ratio=0.05, threshold=0.1):
             res.loc[i, 'class'] = 1
         if box['confidence'] < threshold:
             res.loc[i, 'class'] = 1
-    res = res.drop(index=res[res['class'] == 1].index)
+    res_less = res.drop(index=res[res['class'] == 1].index)
     print("检测结果：\n", res)
-    boxes = [list(map(int, i[1:5])) for i in res.itertuples()]
+    boxes = [list(map(int, i[1:5])) for i in res_less.itertuples()]
     # 假如少于等于3个，直接返回，否则根据多幅图像提取水印
     if len(res) <= 3:
+        print("未使用增强")
         w1, h1, w2, h2 = boxes[0]
         return img_np[h1:h2, w1:w2], boxes
     else:
+        print("增强")
         # 把所有子图都resize到相同大小
         wms = []  # watermarks
         for w1, h1, w2, h2 in boxes:
@@ -64,7 +67,7 @@ def detect_watermark_from_img_result(img, res, err_ratio=0.05, threshold=0.1):
             wms.append(np.array(i))
         # 增强水印
         wm = estimate_watermark_from_images(wms)
-        return wm, boxes
+        return wm, [list(map(int, i[1:5])) for i in res.itertuples()]
 
 
 def estimate_watermark_from_images(imgs: list, enhance: int = 50):
@@ -73,10 +76,24 @@ def estimate_watermark_from_images(imgs: list, enhance: int = 50):
     grad_y = list(map(lambda x: cv2.Sobel(x, cv2.CV_64F, 0, 1, ksize=3), imgs))
     Wm_x = np.median(np.array(grad_x), axis=0)
     Wm_y = np.median(np.array(grad_y), axis=0)
+
+    # plt.subplot(311)  # DEBUG
+    # plt.imshow(np.abs(Wm_x ** 2 + Wm_y ** 2) / np.max(Wm_x ** 2 + Wm_y ** 2))  # DEBUG
+    # ax = plt.gca()
+    # ax.axes.xaxis.set_visible(False)
+    # ax.axes.yaxis.set_visible(False)
+
     est = poisson_reconstruct(Wm_x, Wm_y)
     # 转换成255的
     est: np.ndarray = 255 * (est - np.min(est)) / (np.max(est) - np.min(est))
     est = est.astype(np.uint8)
+    # DEBUG
+    # plt.subplot(312)  # DEBUG
+    # plt.imshow(est)  # DEBUG
+    # ax = plt.gca()
+    # ax.axes.xaxis.set_visible(False)
+    # ax.axes.yaxis.set_visible(False)
+
     # 寻找增强区域的模版
     channels = []
     for i in range(est.shape[-1]):
@@ -100,6 +117,13 @@ def estimate_watermark_from_images(imgs: list, enhance: int = 50):
     est = est + enhance * mask
     est: np.ndarray = 255 * (est - np.min(est)) / (np.max(est) - np.min(est))
     est = est.astype(np.uint8)
+    # DEBUG
+    # plt.subplot(313)
+    # plt.imshow(est)
+    # ax = plt.gca()
+    # ax.axes.xaxis.set_visible(False)
+    # ax.axes.yaxis.set_visible(False)
+    # plt.show()
     return est
 
 
@@ -135,129 +159,134 @@ def poisson_reconstruct(gradx, grady, kernel_size=3, num_iters=100, h=0.1,
         )
         error = np.sum(np.square(est - old_est))
         loss.append(error)
-
     return est
 
 
-# GUI选择图片/视频
-root = tk.Tk()  # 创建一个Tkinter.Tk()实例
-file_path = tkinter.filedialog.askopenfilename(
-    multiple=False,
-    filetypes=[('图片', '.jpg'), ('图片', '.png'), ('视频', '.mp4')]
-)
-# root.withdraw()  # 将Tkinter.Tk()实例隐藏
-file_path = Path(file_path)
-if file_path.is_file() is False:
-    logger.error("未选择资源")
-    exit(0)
-
-# 提取
-if file_path.suffix in ['.jpg', '.png', '.jpeg']:  # 图片
-    file_type = 'image'
-    imgs = [Image.open(file_path).convert('RGB')]
-else:  # 视频（平均抽取10帧）
-    file_type = 'video'
-    video = VideoReader(str(file_path))
-    indices = np.linspace(2, video.frame_cnt - 2, 10).astype(int)
-    imgs = [Image.fromarray(video.get_frame(i)[:, :, ::-1]) for i in indices]
-
-
-# 加载模型
-logger.info("开始加载YoloV5模型")
-model = torch.hub.load('yolov5', 'custom', path='yolov5/best.pt', source='local')
-model = model.cpu()
-matplotlib.use('Qt5Agg')
-logger.info("YoloV5加载成功")
-
-# 检测
-logger.info("检测中")
-results = model(imgs)
-
-# 提取水印
-results = results.pandas().xyxy
-if file_type == 'image':
-    if len(results[0]) == 0:
-        logger.error("Yolo检测失败")
-        exit(0)
-    test_wm, box = detect_watermark_from_img_result(imgs[0], results[0])
-elif file_type == 'video':
-    idx = -1
-    for i, result_item in enumerate(results):
-        if len(result_item) != 0:
-            idx = i
-            break
-    if idx == -1:
-        logger.error("Yolo检测失败")
-        print(results)
-        exit(0)
-    test_wm, box = detect_watermark_from_video_result(imgs, results[idx])
-else:
-    raise ValueError
-
-# 溯源
-api = BaiduAPI('baidu_cfg.json')
-mark_res = api.detect_mark(Image.fromarray(test_wm))
-if mark_res['result_num'] > 0 and mark_res['result'][0]['probability'] >= 0.7:
-    search_res = api.search(mark_res['result'][0]['name'])
-    output = f"检测到可能的水印来源：{mark_res['result'][0]['name']}\n" + \
-             f"以下是详细信息：\n{search_res[0]['title']} \n{search_res[0]['href']} \n{search_res[0]['summary']}\n" + \
-             "获取方式：百度Logo识别+搜索引擎"
-    print(output)
-    # print(f"检测到可能的水印来源：{mark_res['result'][0]['name']}")
-    # print(f"以下是详细信息：\n\t{search_res[0]['title']} \n\t{search_res[0]['href']} \n\t{search_res[0]['summary']}")
-    # print("获取方式：百度Logo识别+搜索引擎")
-else:
-    ocr_res = api.detect_text(Image.fromarray(test_wm))
-    if ocr_res['words_result_num'] >= 1:
-        ocr_words: str = ocr_res['words_result'][0]['words']
-        if '@' in ocr_words:
-            ocr_words = ocr_words[ocr_words.index('@'):]
-        search_res = api.search(ocr_words)
-        # print(f"检测到可能的水印来源：{ocr_words}")
-        # print(f"以下是详细信息：\n\t{search_res[0]['title']} \n\t{search_res[0]['href']} \n\t{search_res[0]['summary']}")
-        # print("获取方式：百度OCR+搜索引擎")
-        output = f"检测到可能的水印来源：{ocr_words}\n" + \
-                 f"以下是详细信息：\n{search_res[0]['title']} \n{search_res[0]['href']} \n{search_res[0]['summary']}\n" + \
-                 "获取方式：百度OCR+搜索引擎"
-        print(output)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-m', '--image', required=False, type=str, help="输入图片路径，假如没有则调用tkinter")
+    args = parser.parse_args()
+    if args.image is not None:
+        file_path = args.image
     else:
-        output = "溯源失败"
+        # GUI选择图片/视频
+        root = tk.Tk()  # 创建一个Tkinter.Tk()实例
+        file_path = tkinter.filedialog.askopenfilename(
+            multiple=False,
+            filetypes=[('图片', '.jpg'), ('图片', '.png'), ('视频', '.mp4')]
+        )
+    file_path = Path(file_path)
+    if file_path.is_file() is False:
+        logger.error("未选择资源")
+        exit(0)
 
-# 展示
-new_img = np.array(imgs[0])
-for point in box:
-    rr, cc = draw.rectangle_perimeter(point[:2], end=point[2:], shape=imgs[0].size)
-    new_img[cc, rr] = (0, 255, 255)
-new_img = Image.fromarray(new_img)
-# plt.figure(1)
-#
-# plt.subplot(121)
-# plt.imshow(new_img)
-#
-# plt.subplot(222)
-# plt.imshow(test_wm)
-#
-# plt.subplot(224)
-# ax = plt.gca()
-# text_box = TextBox(ax, "结果：", initial=output)
-#
-# plt.show()
+    # 提取
+    if file_path.suffix in ['.jpg', '.png', '.jpeg']:  # 图片
+        file_type = 'image'
+        imgs = [Image.open(file_path).convert('RGB')]
+    else:  # 视频（平均抽取10帧）
+        file_type = 'video'
+        video = VideoReader(str(file_path))
+        indices = np.linspace(2, video.frame_cnt - 2, 10).astype(int)
+        imgs = [Image.fromarray(video.get_frame(i)[:, :, ::-1]) for i in indices]
 
-# 展示2
-# draw_win(root, new_img, Image.fromarray(test_wm), output)
-frame_l = tk.Frame(master=root, relief=tk.RAISED, borderwidth=1)
-frame_l.grid(row=0, column=0)
-_new_img = new_img.resize((int(new_img.width / new_img.height * 400), 400))
-_source_photo = ImageTk.PhotoImage(_new_img)
-label1 = tk.Label(master=frame_l, image=_source_photo)
-label1.pack()
+    # 加载模型
+    logger.info("开始加载YoloV5模型")
+    model = torch.hub.load('yolov5', 'custom', path='yolov5/best.pt', source='local')
+    model = model.cpu()
+    matplotlib.use('Qt5Agg')
+    logger.info("YoloV5加载成功")
 
-frame_r = tk.Frame(master=root, relief=tk.RAISED, borderwidth=1)
-frame_r.grid(row=0, column=1)
-_test_wm = Image.fromarray(test_wm)
-_photo = ImageTk.PhotoImage(_test_wm)
-label2 = tk.Label(master=frame_r, image=_photo, bg='gray')
-label2.grid(row=0, column=0)
-label3 = tk.Label(master=frame_r, text=output, justify=tk.LEFT, wraplength=300)  # , width=30
-label3.grid(row=1, column=0)
-root.mainloop()
+    # 检测
+    logger.info("检测中")
+    results = model(imgs)
+
+    # 提取水印
+    results = results.pandas().xyxy
+    if file_type == 'image':
+        if len(results[0]) == 0:
+            logger.error("Yolo检测失败")
+            exit(0)
+        test_wm, box = detect_watermark_from_img_result(imgs[0], results[0])
+    elif file_type == 'video':
+        idx = -1
+        for i, result_item in enumerate(results):
+            if len(result_item) != 0:
+                idx = i
+                break
+        if idx == -1:
+            logger.error("Yolo检测失败")
+            print(results)
+            exit(0)
+        test_wm, box = detect_watermark_from_video_result(imgs, results[idx])
+    else:
+        raise ValueError
+
+    # 溯源
+    api = BaiduAPI('baidu_cfg.json')
+    mark_res = api.detect_mark(Image.fromarray(test_wm))
+    if mark_res['result_num'] > 0 and mark_res['result'][0]['probability'] >= 0.7:
+        search_res = api.search(mark_res['result'][0]['name'])
+        output = f"检测到可能的水印来源：{mark_res['result'][0]['name']}\n" + \
+                 f"以下是详细信息：\n{search_res[0]['title']} \n{search_res[0]['href']} \n{search_res[0]['summary']}\n" + \
+                 "获取方式：百度Logo识别+搜索引擎"
+        print(output)
+        # print(f"检测到可能的水印来源：{mark_res['result'][0]['name']}")
+        # print(f"以下是详细信息：\n\t{search_res[0]['title']} \n\t{search_res[0]['href']} \n\t{search_res[0]['summary']}")
+        # print("获取方式：百度Logo识别+搜索引擎")
+    else:
+        ocr_res = api.detect_text(Image.fromarray(test_wm))
+        if ocr_res['words_result_num'] >= 1:
+            ocr_words: str = ocr_res['words_result'][0]['words']
+            if '@' in ocr_words:
+                ocr_words = ocr_words[ocr_words.index('@'):]
+            search_res = api.search(ocr_words)
+            # print(f"检测到可能的水印来源：{ocr_words}")
+            # print(f"以下是详细信息：\n\t{search_res[0]['title']} \n\t{search_res[0]['href']} \n\t{search_res[0]['summary']}")
+            # print("获取方式：百度OCR+搜索引擎")
+            output = f"检测到可能的水印来源：{ocr_words}\n" + \
+                     f"以下是详细信息：\n{search_res[0]['title']} \n{search_res[0]['href']} \n{search_res[0]['summary']}\n" + \
+                     "获取方式：百度OCR+搜索引擎"
+            print(output)
+        else:
+            output = "溯源失败"
+
+    # 展示
+    new_img = np.array(imgs[0])
+    for point in box:
+        rr, cc = draw.rectangle_perimeter(point[:2], end=point[2:], shape=imgs[0].size)
+        new_img[cc, rr] = (0, 255, 255)
+    new_img = Image.fromarray(new_img)
+    # plt.figure(1)
+    #
+    # plt.subplot(121)
+    # plt.imshow(new_img)
+    #
+    # plt.subplot(222)
+    # plt.imshow(test_wm)
+    #
+    # plt.subplot(224)
+    # ax = plt.gca()
+    # text_box = TextBox(ax, "结果：", initial=output)
+    #
+    # plt.show()
+
+    # 展示2
+    # draw_win(root, new_img, Image.fromarray(test_wm), output)
+    if args.image is None:
+        frame_l = tk.Frame(master=root, relief=tk.RAISED, borderwidth=1)
+        frame_l.grid(row=0, column=0)
+        _new_img = new_img.resize((int(new_img.width / new_img.height * 400), 400))
+        _source_photo = ImageTk.PhotoImage(_new_img)
+        label1 = tk.Label(master=frame_l, image=_source_photo)
+        label1.pack()
+
+        frame_r = tk.Frame(master=root, relief=tk.RAISED, borderwidth=1)
+        frame_r.grid(row=0, column=1)
+        _test_wm = Image.fromarray(test_wm)
+        _photo = ImageTk.PhotoImage(_test_wm)
+        label2 = tk.Label(master=frame_r, image=_photo, bg='gray')
+        label2.grid(row=0, column=0)
+        label3 = tk.Label(master=frame_r, text=output, justify=tk.LEFT, wraplength=300)  # , width=30
+        label3.grid(row=1, column=0)
+        root.mainloop()
